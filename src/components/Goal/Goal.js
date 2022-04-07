43f5f74b53653d5ebee65cdbb17961ef879420c8
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-04-07 14:14:58 +0600

    simple animation add

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -4,52 +4,54 @@ const Goal = () => {
   return (
     <div>
       <div className="container">
-                <h1>Our Goal</h1>
+        <h1 className="text-center">Our Goal</h1>
         <div class="row g-4 my-5">
-          <div class="col-md-6 col-lg-6   "
-          data-aos="fade-down"
-          data-aos-offset="300"
-          data-aos-delay="5000"
-          data-aos-duration="2000"
-          data-aos-easing="ease-in-out"
-          data-aos-mirror="true"
-          data-aos-once="false"
-          // data-aos-anchor-placement="top-center"  
-          
+          <div
+            class="col-md-6 col-lg-6   "
+            data-aos="fade-up"
+            data-aos-offset="300"
+            data-aos-delay="5000"
+            data-aos-duration="3000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+            // data-aos-anchor-placement="top-center"
           >
             <div class="p-3   text-light border rounded-3 border bg-dark">
-              <h5 className="text-light">What is semantic tag?</h5>
+              <h5 className="text-light">Our Goal?</h5>
               <p>
-                Semantic tag is meaningfull element thos that perfectlly
-                describe their meaning in human and machine readable.Such as
-                section,article,
-                footer,aside,header,main,summary,details,nav,figcaption.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam,
+                architecto. Voluptate nesciunt adipisci, sapiente accusantium
+                optio eaque perferendis est, dolores voluptas molestias quos!
+                Commodi possimus itaque quis vero quidem officia velit officiis
+                iste? Quia quidem fugit velit aliquid totam! Sed ex quos cum
+                nemo velit magnam odio perferendis optio eligendi. Assumenda
+                provident magni accusamus quia, cumque laudantium quas sed ipsum
+                consectetur ea eveniet odio eos fugit hic harum rem
+                exercitationem? Eius, voluptatum aliquid. Non, mollitia fuga
+                labore eligendi id necessitatibus magnam delectus autem optio
+                tenetur ratione assumenda nisi. Totam accusantium recusandae
+                aut, blanditiis quaerat veniam consectetur ipsum commodi quos
+                nesciunt?
               </p>
             </div>
           </div>
 
-          <div class="col-md-6 col-lg-6 text-light"
-          data-aos="zoom-in-up"
-          data-aos-offset="300"
-          data-aos-delay="5000"
-          data-aos-duration="2000"
-          // data-aos-easing="ease-in-out"
-          // data-aos-mirror="true"
-          // data-aos-once="false"
-          
+          <div
+            class="col-md-6 col-lg-6 text-light"
+            data-aos="zoom-in-up"
+            data-aos-offset="300"
+            data-aos-delay="5000"
+            data-aos-duration="2000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
           >
             <div class="p-3   align-items-center justify-content-between border rounded-3 border bg-dark">
-              <h5 className="">What is purpose of context api?</h5>
-              <p>
-                Without having to pass props down manually at every
-                label.Context provides a way to pass data from parent to child
-                component.
-              </p>
-              <li>Create context.</li>
-              <li>Wrap parent context provide. </li>
-              <li>Provide a value of the context as props.</li>
-              <li>Wrap child with context consumer.</li>
-              <li>Consumer follows the render props pattern.</li>
+              <img
+                src="https://images.unsplash.com/photo-1629568853783-d94e53ee53d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80"
+                alt="" className="img-fluid"
+              />
             </div>
           </div>
         </div>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -12,6 +12,9 @@ const Products = () => {
               data-aos="fade-right"
           //     data-aos-delay="5000"
               data-aos-duration="3000"
+              data-aos-easing="ease-in-out"
+               data-aos-mirror="true"
+            data-aos-once="false"
             >
               <img
                 src="https://images.unsplash.com/photo-1484704849700-f032a568e944?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
@@ -31,7 +34,11 @@ const Products = () => {
               </div>
             </div>
           </div>
-          <div className="col " data-aos="fade-down" data-aos-duration="3000">
+          <div className="col " data-aos="fade-down" data-aos-duration="3000" 
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+       data-aos-once="false"
+          >
             <div className="card h-100">
               <img
                 src="https://images.unsplash.com/photo-1481207801830-97f0f9a1337e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
@@ -50,7 +57,11 @@ const Products = () => {
               </div>
             </div>
           </div>
-          <div className="col" data-aos="fade-left" data-aos-duration="3000">
+          <div className="col" data-aos="fade-left" data-aos-duration="3000"
+           data-aos-easing="ease-in-out"
+           data-aos-mirror="true"
+        data-aos-once="false"
+          >
             <div className="card h-100">
               <img
                 src="https://images.unsplash.com/photo-1516924962500-2b4b3b99ea02?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
